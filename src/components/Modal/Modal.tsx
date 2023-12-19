184a33f6da68a2731f24b4cae17dbb28a60035c7
nullclarity <ViktorovDmitr@yandex.ru>
2023-12-19 19:04:55 +0300

    init branch

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -20,7 +20,6 @@ const Modal: React.FC<ModalProps> = ({ className, item}) => {
 
     const [modal, setModal] = useState<boolean>(false);
 
-
     const toggleModal = () => {
         setModal(!modal);
     }
